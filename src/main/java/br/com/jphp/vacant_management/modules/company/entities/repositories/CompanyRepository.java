6ec8cad82125848110efe7ad11133549a4c8b315
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2025-09-20 10:44:51 -0300

    .

---
+++ src/main/java/br/com/jphp/vacant_management/modules/company/entities/repositories/CompanyRepository.java
@@ -0,0 +1,11 @@
+package br.com.jphp.vacant_management.modules.company.entities.repositories;
+
+import br.com.jphp.vacant_management.modules.company.entities.CompanyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID> {
+    Optional<CompanyEntity> findByUsernameOrEmail(String username, String email);
+}
